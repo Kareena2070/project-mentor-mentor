35fe41dae16f7d6ec36d3a617123c71e77e2d52c
Kareena2070 <kareenayadav1088@gmail.com>
2025-09-12 09:51:16 +0530

    Add images and new CSS styling for Home page cards and header layout

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,15 +1,24 @@
+// add css file
+import '../pages/home.css';
 
+// / Import all three images individually
+import reflectionImg from '../assets/reflection.png';
+import feedbackImg from '../assets/feedback.png';
+import growthImg from '../assets/growth.png';
 function Home() {
     const explinList = [
         {
+            icone: reflectionImg,
             title: "Daily Reflection",
             description: "Log what you learned each day to improve retention and track your growth journey"
         },
         {
+            icone: feedbackImg,
             title: "Mutual Feedback",
             description: "Give and receive feedback to build confidence and accountability in your learning."
         }, 
-        {
+        {   
+            icone: growthImg,
             title: "Visual Growth",
             description: "See your progress with charts, levels, and streaks that make learning motivating."
         }
@@ -17,22 +26,27 @@ function Home() {
 
     return (
         <>
+        <div className="main-container">
             <div className="header">
                 <h1>Welcome to NavGurukul</h1>
                 <p>Mentor-Mentee Progress Tracker</p>
                 <p>Track your learning journey, measure growth, and celebrate progress together. Built for pair programming excellence at NavGurukul.</p>
-                <button> 📝Add Today's Learning</button>
-                <button> 📊View Progress</button>
+                <div className='head-button'>
+                    <button className='head-buton-1'> Add Today's Learning</button>
+                    <button className='head-buton-2'> View Progress</button>
+                </div>
             </div>
             
             <div className="cards-container">
                 {explinList.map((explain, id) => (
                     <div key={id} className="card">
+                        <img src={explain.icone} alt={explain.title} />
                         <h2>{explain.title}</h2>
                         <p>{explain.description}</p>
                     </div>
                 ))}
             </div>
+        </div>
         </>
     );
 }
